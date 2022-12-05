53af26fa3d8a65f35c3242ef57fc77677ed6c6da
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-12-04 21:57:07 -0700

    finished inquirer and main js files

--- index.js
+++ index.js
@@ -98,10 +98,87 @@ const employeeQuestion = () => {
         }
         team.push(employee);
         if (confirmAdd) {
-            return promptEmployee(team);
+            return employeeQuestion(team);
         } else {
             console.log(team);
             return team;
         }
     })
 };
+
+const generateWebpage = () => {
+    outputHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Team!</title>
+            <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        </head>
+        <body>
+            <section id="services">
+                <div class="container">
+                    <h1 class="text-center">Team</h1>
+                </div>
+            </section>
+            <section class="container d-flex flex-wrap gap-5 justify-content-center">`;
+    team.forEach(employee => {
+        generateProfile(employee);
+    });
+    outputHTML +=  `
+        </section>
+        </body>
+    </html>
+    `;
+    fs.writeFile(`./public/team.html`, outputHTML, error => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(`Team Created!`);
+        }
+    });
+};
+
+const generateProfile = employee => {
+    outputHTML += `
+    <div class="card">
+        <div class="card-header bg-dark text-white">
+            <h2>${employee.name}</h2>
+            <div class="title">${employee.getRole()}</div>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <tr>
+                    <td>ID: ${employee.id}</td>
+                </tr>
+                <tr>
+                    <td>eMail: <a href="mailto:${employee.email}">${employee.email}</a></td>
+                </tr>
+                <tr>`;
+    switch (employee.getRole()) {
+        case 'Manager':
+            outputHTML += `<td>Office Number: ${employee.officeNumber}</td>`;
+            break;
+        case 'Engineer':
+            outputHTML += `<td>GitHub: <a href="https://www.github.com/${employee.github}/">${employee.github}</a></td>`;
+            break;
+        case 'Intern':
+            outputHTML += `<td>School: ${employee.school}</td>`;
+            break;
+    }
+    outputHTML += `
+                </tr>
+            </table>
+        </div>
+    </div>`
+};
+
+managerQuestion().then(employeeQuestion).then(team => {
+    return generateWebpage(team);
+}).catch(err => {
+    console.log(err);
+});
